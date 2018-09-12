4cab7ca77757fb2c3b8b0812261a91846725d87f
MutumaMutuma <lewismutuma1000@gmail.com>
2018-09-12 15:39:25 +0300

    added a new template

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager 
+from flask_mail import Mail
 
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+mail = Mail()
 
 bootstrap = Bootstrap()
 
@@ -22,7 +24,9 @@ def create_app(config_name):
     db.init_app(app)
     bootstrap = Bootstrap(app)
     login_manager.init_app(app)
-    
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
     # registering the main app blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 export SECRET_KEY='01570936bd6b231dd1e1037bb224cd'
+export MAIL_USERNAME='lewismutuma1000@gmail.com'
+export MAIL_PASSWORD='Ilovememore100'
 
 python3.6 manage.py server
