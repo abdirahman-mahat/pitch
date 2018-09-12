2f8468c1802b40dfd06d9797000f63c93dfcd783
MutumaMutuma <lewismutuma1000@gmail.com>
2018-09-12 12:24:09 +0300

    added emails.py

--- app/emails.py
+++ app/emails.py
@@ -3,7 +3,7 @@ from flask import render_template
 from . import mail
 
 def mail_message(subject,template,to,**kwargs):
-   sender_email = 'lewismutuma1000@gmail.com'
+   sender_email = ''
 
    email = Message(subject, sender=sender_email, recipients=[to])
    email.body= render_template(template + ".txt",**kwargs)
