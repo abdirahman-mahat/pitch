bd485635f5546a5470d3fbbbd19645abf587c463
MutumaMutuma <lewismutuma1000@gmail.com>
2018-09-12 12:27:34 +0300

    added class comments

--- app/models.py
+++ app/models.py
@@ -50,7 +50,7 @@ class Pitch(db.Model):
         return f'{self.title}'
 
 class Comment(db.Model):
-    __tablename__=''
+    __tablename__='comments'
 
     id = db.Column(db.Integer,primary_key=True)
     comment_content = db.Column(db.String())
