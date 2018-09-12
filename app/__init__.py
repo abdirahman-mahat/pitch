897a5c3e8ac887f67b4897e0e0fdb50656092674
MutumaMutuma <lewismutuma1000@gmail.com>
2018-09-12 12:25:57 +0300

    added flask mail

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask_login import LoginManager
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+
 bootstrap = Bootstrap()
 
 db = SQLAlchemy()
