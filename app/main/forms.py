56bfd1079c8d49b3e9c975af8b94549706a06a81
MutumaMutuma <lewismutuma1000@gmail.com>
2018-09-13 10:28:08 +0300

    added pitch forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,7 +6,7 @@ class PitchForm(FlaskForm):
     title = StringField('Pitch title')
     category_id = SelectField('Pitch Category', choices=[('Select a category', 'Select a category'),('Business', 'Business'),('Science and Tech', 'Science and Tech'),('Interview Pitch', 'Interview Pitch')])
     content = TextAreaField('The pitch...')
-    submit = SubmitField('Submit')
+    submit = SubmitField('Post Pitch')
 
 
 class CommentForm(FlaskForm):
--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from . import main
-from flask import render_template,request,redirect,url_for,abort
+from flask import render_template,request,redirect,url_for,abort,flash
 from flask_login import login_required
 from ..models import User, Pitch, Comment
-from .forms import UpdateProfile
-
+from .forms import UpdateProfile, PitchForm
+from .. import db, photos
 app = Flask(__name__)
 
 
@@ -43,3 +43,38 @@ def update_profile(uname):
         return redirect(url_for('.profile',uname=user.username))
 
     return render_template('profile/update.html',form =form)
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+
+@main.route('/pitch/new', methods=['GET','POST'])
+@login_required
+def new_pitch():
+    form = PitchForm()
+
+    if form.validate_on_submit():
+
+        title=form.title.data
+        content=form.content.data
+        category_id=form.category_id.data
+        pitch = Pitch(title=title,
+                      content=content,
+                      category_id=category_id,
+                      user=current_user)
+
+        db.session.add(pitch)
+        db.session.commit()
+
+        # pitch.save_pitch(pitch)
+        
+        flash('Your pitch has been created!', 'success')
+        return redirect(url_for('main.single_pitch',id=pitch.id))
+
+    return render_template('new_pitch.html', title='New Post', pitch_form=form, post ='New Post')
