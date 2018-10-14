d7be92aecd87e363c0cf1d4ebef09fa700ccc0f0
abdirahman mahat <abdirahmanmahat3@gmail.com>
2018-10-14 20:18:30 +0300

    update templates

--- config.py
+++ config.py
@@ -3,6 +3,12 @@ import os
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://abdirahman:1234@localhost/pitch'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("abdirahmanmahat3@gmail.com")
+    MAIL_PASSWORD = os.environ.get("abdirahmanun11")
 class prodConfig(Config):
     pass
 class DevConfig(Config):
---
+++ start.sh
@@ -0,0 +1,3 @@
+export MAIL_USERNAME=abdirahmanmahat3@gmail.com
+export MAIL_PASSWORD=abdirahmanun11
+python3.6 manage.py server
