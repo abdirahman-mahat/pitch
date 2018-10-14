9f081796c63c029c3ae9cfb24d5d55f9bb65e12f
abdirahman mahat <abdirahmanmahat3@gmail.com>
2018-10-14 20:53:34 +0300

    add photo destination

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
+    SECRET_kEY = '1234'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://abdirahman:1234@localhost/pitch'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
@@ -9,11 +10,27 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("abdirahmanmahat3@gmail.com")
     MAIL_PASSWORD = os.environ.get("abdirahmanun11")
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
 class prodConfig(Config):
-    pass
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    
+    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://abdirahman:1234@localhost/pitch_test'
+
+
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://abdirahman:1234@localhost/pitch'
+
     DEBUG = True
+
 config_options = {
-    'development':DevConfig,
-    'production':prodConfig
+   
+    'development': DevConfig,
+    'production': prodConfig,
+    'test': TestConfig
 }
--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User
+from app.models import *
 from flask_migrate import Migrate,MigrateCommand
 # creating app instance
 app = create_app('development')
-# app = create_app('test')
+app = create_app('test')
 app = create_app('production')
 
 manager = Manager(app)
